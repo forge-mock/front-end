333e63d8ed3aeab945e240bd7f856ef323719646
Khavol <olehkhavar@gmail.com>
2025-01-06 20:15:27 +0200

    remove colors from icons

--- src/widgets/layout/sidebar/components/tab-button/tab-button.tsx
+++ src/widgets/layout/sidebar/components/tab-button/tab-button.tsx
@@ -7,17 +7,18 @@ import { Tab as TabProps } from "../../lib/interfaces/tab";
 
 function TabButton({ icon, title, path }: TabProps): React.JSX.Element {
   return (
-    <Link href={path}>
-      <div className="p-2.5">
-        <div
-          className="flex flex-row items-center gap-4 p-1 border-2 border-solid border-slate-100 rounded-xl
-          transition duration-300 hover:bg-slate-300 active:transition-none active:bg-slate-400"
-        >
-          <Icon src={icon} alt={title} />
-          <p className="text-violet-800 truncate">{title}</p>
-        </div>
-      </div>
-    </Link>
+    <div className="p-2.5">
+      <Link
+        href={path}
+        className={`flex flex-row items-center gap-4 p-1 border-2 border-solid border-slate-100 rounded-xl
+          transition duration-300 hover:bg-slate-300 active:transition-none active:bg-slate-400
+          focus-visible:transition focus-visible:duration-300 focus-visible:outline-none focus-visible:ring-offset-2 
+          focus-visible:ring focus-visible:ring-blue-500`}
+      >
+        <Icon src={icon} alt={title} classes="border-blue-500" />
+        <p className="text-violet-800 truncate">{title}</p>
+      </Link>
+    </div>
   );
 }
 
