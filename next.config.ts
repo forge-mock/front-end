be6374bee3b6f96db72dd4ac0e9ad56fcec78ac8
Khavol <olehkhavar@gmail.com>
2025-01-08 23:11:11 +0200

    add content securiry police

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "standalone",
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self' 'unsafe-inline'; script-src 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; img-src 'self' 'unsafe-inline'; connect-src 'self' 'unsafe-inline'; object-src 'none'; frame-ancestors 'none'",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
