370487cdbd95445ee769f0a83ef7ec9d1f49c7d9
Oleh Khavar <olehkhavar@gmail.com>
2025-03-21 19:40:39 +0200

    fix turbopack configuration

--- next.config.ts
+++ next.config.ts
@@ -44,10 +44,21 @@ const nextConfig: NextConfig = {
   },
   webpack(config) {
     config.module.rules.push({
-      test: /\.svg$/,
-      use: ['@svgr/webpack'],
+      test: /\.svg$/i,
+      use: ["@svgr/webpack"],
     });
+
     return config;
+  },
+  experimental: {
+    turbo: {
+      rules: {
+        "*.svg": {
+          loaders: ["@svgr/webpack"],
+          as: "*.js",
+        },
+      },
+    },
   },
 };
 
