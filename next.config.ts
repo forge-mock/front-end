15cdc9bfdceee33f38ff7c088242837fcf6a862e
Oleh Khavar <olehkhavar@gmail.com>
2025-03-21 19:15:24 +0200

    move icons

--- next.config.ts
+++ next.config.ts
@@ -42,13 +42,11 @@ const nextConfig: NextConfig = {
       },
     ];
   },
-  webpack(config, { isServer }) {
-    if (!isServer) {
-      config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-      });
-    }
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
     return config;
   },
 };
