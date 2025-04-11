1173d4c82236b5cf094c7a073cba97e4d4c6a0d5
Oleh Khavar <olehkhavar@gmail.com>
2025-04-11 19:42:14 +0300

    add default login modal appearence

--- src/features/auth-modal/auth-modal.tsx
+++ src/features/auth-modal/auth-modal.tsx
@@ -11,8 +11,14 @@ export interface AuthModalProps {
 
 function AuthModal({ isLogin = true, isOpen = false, setIsOpen }: Readonly<AuthModalProps>): React.JSX.Element {
   return (
-    <Modal isOpen={isOpen}>
-      {isLogin ? <LoginForm setIsOpen={setIsOpen} /> : <RegisterForm setIsOpen={setIsOpen} />}
+    <Modal isOpen={isOpen} setIsOpen={setIsOpen}>
+      <div className="w-64">
+        <div className="flex w-full items-center justify-center text-2xl font-bold italic text-[var(--violet-title-color)] mb-4">
+          {isLogin ? "Login" : "Register"}
+        </div>
+
+        {isLogin ? <LoginForm setIsOpen={setIsOpen} /> : <RegisterForm setIsOpen={setIsOpen} />}
+      </div>
     </Modal>
   );
 }
--- src/features/auth-modal/components/login.tsx
+++ src/features/auth-modal/components/login.tsx
@@ -1,21 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-aria-components";
-import { Input, Button } from "@shared/components";
+import { Input, Button, addToast } from "@shared/components";
+import { formatErrorMessages } from "@shared/api";
+import { login, Login } from "@entities/auth";
+import styles from "./form.module.scss";
 
 export interface LoginFormProps {
   setIsOpen: (isOpen: boolean) => void;
 }
 
 function LoginForm({ setIsOpen }: Readonly<LoginFormProps>): React.JSX.Element {
-  const handleSubmit = () => {
-    setIsOpen(false);
+  const [userInput, setUserInput] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    const loginData: Login = { userInput, password };
+
+    try {
+      const response = await login(loginData);
+
+      if (response.isSuccess) {
+        setIsOpen(false);
+      } else {
+        addToast(formatErrorMessages(response.errors), "error");
+      }
+    } catch {
+      addToast("Login error", "error");
+    }
   };
 
   return (
-    <Form onSubmit={handleSubmit}>
-      <Input label="Username" />
-      <Input label="Password" type="password" />
-      <Button text="Login" type="submit" />
+    <Form onSubmit={handleSubmit} className={styles.form}>
+      <Input label="Username / email" value={userInput} onChange={setUserInput} />
+      <Input label="Password" type="password" value={password} onChange={setPassword} />
+      <Button text="Login" type="submit" classes={styles.button} />
     </Form>
   );
 }
