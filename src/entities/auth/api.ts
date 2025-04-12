63aac3f918a6706885c4f7a69d3f9e95037a7a91
Oleh Khavar <olehkhavar@gmail.com>
2025-04-12 14:54:58 +0300

    fix logout error

--- src/entities/auth/api.ts
+++ src/entities/auth/api.ts
@@ -4,11 +4,11 @@ import type { Login, Register } from "./interfaces";
 const auth = "/auth";
 
 export function login(login: Login): Promise<ApiResponse<string>> {
-  return noAuthApi.post(`${auth}/authenticate`, login);
+  return noAuthApi.post(`${auth}/authenticate`, login, { withCredentials: true });
 }
 
 export function register(register: Register): Promise<ApiResponse<string>> {
-  return noAuthApi.post(`${auth}/register`, register);
+  return noAuthApi.post(`${auth}/register`, register, { withCredentials: true });
 }
 
 export function logout(accessToken: string): Promise<ApiResponse<boolean>> {
