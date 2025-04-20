7e9766734708361029b3472ed79240b1651b7d3e
Oleh Khavar <olehkhavar@gmail.com>
2025-04-20 19:34:15 +0300

    add strict transport security

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,10 @@ const nextConfig: NextConfig = {
             key: "X-Content-Type-Options",
             value: "nosniff",
           },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
         ],
       },
     ];
