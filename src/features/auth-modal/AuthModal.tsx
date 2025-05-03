611ec2141985239c886b90a67790a8d159095184
Oleh Khavar <olehkhavar@gmail.com>
2025-05-03 22:29:12 +0300

    add oauth logos

--- src/features/auth-modal/AuthModal.tsx
+++ src/features/auth-modal/AuthModal.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { signIn } from "next-auth/react";
 import { Form } from "react-aria-components";
+import GoogleIcon from "@assets/oauth-providers/google.svg";
+import GithubIcon from "@assets/oauth-providers/github.svg";
 import { formatErrorMessages } from "@shared/api";
 import { LOCAL_STORAGE_ITEMS } from "@shared/constants";
 import { setLocalStorageItem } from "@shared/helpers";
@@ -17,6 +19,8 @@ export interface AuthModalProps {
   setIsOpen: (isOpen: boolean) => void;
 }
 
+const divider = <div className="h-[1px] w-1/4 bg-[var(--text-color)]" />;
+
 function AuthModal({
   isLogin = true,
   isOpen = false,
@@ -24,15 +28,6 @@ function AuthModal({
   setIsOpen,
 }: Readonly<AuthModalProps>): React.JSX.Element {
   const { setIsLoggedIn } = useLoginStore();
-
-  const handleLogin = async () => {
-    await signIn("google");
-  };
-
-  const handleGitHubLogin = async () => {
-    await signIn("github");
-  };
-
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { values, errors, isFieldInvalid, handleChange, validate, reset } = useFormValidation(
     isLogin ? LOGIN_SCHEMA : REGISTER_SCHEMA
@@ -72,6 +67,10 @@ function AuthModal({
     }
   }
 
+  async function handleOauthLogin(provider: string): Promise<void> {
+    await signIn(provider);
+  }
+
   function handleSwitchForm(e: React.MouseEvent): void {
     e.preventDefault();
     setIsLogin(!isLogin);
@@ -80,44 +79,55 @@ function AuthModal({
 
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen} title={isLogin ? "Login" : "Register"}>
-      <Form onSubmit={handleSubmit} className="flex flex-col w-96 gap-4">
-        {!isLogin && (
+      <Form onSubmit={handleSubmit} className="flex flex-col w-96">
+        <div className="flex flex-col gap-4">
+          {!isLogin && (
+            <Input
+              label="Username"
+              value={values.username ?? ""}
+              errorMessage={errors.username}
+              isInvalid={isFieldInvalid(REGISTER_FIELDS.username)}
+              onChange={(value) => handleChange(REGISTER_FIELDS.username, value)}
+            />
+          )}
+
           <Input
-            label="Username"
-            value={values.username ?? ""}
-            errorMessage={errors.username}
-            isInvalid={isFieldInvalid(REGISTER_FIELDS.username)}
-            onChange={(value) => handleChange(REGISTER_FIELDS.username, value)}
+            label="Email"
+            type="email"
+            value={values.userEmail ?? ""}
+            errorMessage={errors.userEmail}
+            isInvalid={isFieldInvalid(LOGIN_FIELDS.userEmail)}
+            onChange={(value) => handleChange(LOGIN_FIELDS.userEmail, value)}
           />
-        )}
 
-        <Input
-          label="Email"
-          type="email"
-          value={values.userEmail ?? ""}
-          errorMessage={errors.userEmail}
-          isInvalid={isFieldInvalid(LOGIN_FIELDS.userEmail)}
-          onChange={(value) => handleChange(LOGIN_FIELDS.userEmail, value)}
-        />
+          <Input
+            label="Password"
+            type="password"
+            value={values.password ?? ""}
+            errorMessage={errors.password}
+            isInvalid={isFieldInvalid(LOGIN_FIELDS.password)}
+            onChange={(value) => handleChange(LOGIN_FIELDS.password, value)}
+          />
+        </div>
 
-        <Input
-          label="Password"
-          type="password"
-          value={values.password ?? ""}
-          errorMessage={errors.password}
-          isInvalid={isFieldInvalid(LOGIN_FIELDS.password)}
-          onChange={(value) => handleChange(LOGIN_FIELDS.password, value)}
-        />
+        <div className="flex flex-row items-center justify-center mt-8">
+          {divider}
+          <p className="text-center mx-2">Or sign {isLogin ? "in" : "up"} with</p>
+          {divider}
+        </div>
+
+        <div className="w-100 items-center flex justify-center gap-4 mb-4">
+          <GoogleIcon width={50} height={50} onClick={() => handleOauthLogin("google")} className="cursor-pointer" />
+          <div className="rounded-full bg-[#f1f5f9]">
+            <GithubIcon width={50} height={50} onClick={() => handleOauthLogin("github")} className="cursor-pointer" />
+          </div>
+        </div>
 
         <button type="button" onClick={handleSwitchForm} className="cursor-pointer text-center my-2 underline">
           {isLogin ? "Do not have an account? Sign up" : "Already have an account? Sign in"}
         </button>
 
         <div className="flex justify-center gap-4 w-full">
-          <Button text="Google" outline type="button" onPress={handleLogin} />
-
-          <Button text="Github" outline type="button" onPress={handleGitHubLogin} />
-
           <Button text="Cancel" outline type="button" onPress={() => setIsOpen(false)} />
 
           <Button
