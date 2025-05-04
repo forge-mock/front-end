9641915e8d2750848c2b65d3c2bc8bbd613763ad
ArtemHolikov <art081203@gmail.com>
2025-05-04 20:00:30 +0300

    resolved conflict and made up-to-date branch

--- src/features/type-select-modal/components/types-configurator/primitive-configurator/DateTimeConfig.tsx
+++ src/features/type-select-modal/components/types-configurator/primitive-configurator/DateTimeConfig.tsx
@@ -1,4 +1,8 @@
-import { Button, Modal } from "@shared/components";
+import { Button, Modal, Selector } from "@shared/components";
+import { useState } from "react";
+import BlankSlider from "@features/blank-slider/BlankSlider";
+import { dateFormat, separators } from "./constants";
+import { Key, Label } from "react-aria-components";
 
 interface ConfiguratorProps {
   isOpen: boolean;
@@ -6,9 +10,43 @@ interface ConfiguratorProps {
 }
 
 function DateTimeConfig({ isOpen, setIsOpen }: Readonly<ConfiguratorProps>) {
+  const [blankValue, setBlankValue] = useState<number | number[]>(0);
+  const [selectedDateFormat, setSelectedDateFormat] = useState<Key>(dateFormat[0].id);
+  const [selectedSeparator, setSelectedSeparator] = useState<Key>(separators[1].id);
+
+  const changeDateFormat = (selectedFormat: Key) => {
+    setSelectedDateFormat(selectedFormat);
+  };
+
+  const changeSeparator = (separator: Key) => {
+    setSelectedSeparator(separator);
+  };
+
   return (
     <Modal isOpen={isOpen} setIsOpen={setIsOpen} title="DateTime configurator">
-      DateTime
+      <div className="mt-10">
+        <BlankSlider blankValue={blankValue} setBlankValue={setBlankValue} />
+        <div className="flex gap-7 mt-14">
+          <Label className="text-black">Format</Label>
+          <Selector
+            items={dateFormat}
+            onSelectionChange={(selectedFormat) => changeDateFormat(selectedFormat)}
+            placeholder="Select date format"
+            selectedKey={selectedDateFormat}
+            selectorClasses="w-[50%]"
+          />
+        </div>
+        <div className="flex gap-2 mt-6">
+          <Label className="text-black">Separator</Label>
+          <Selector
+            items={separators}
+            onSelectionChange={(separator) => changeSeparator(separator)}
+            placeholder="Select date format"
+            selectedKey={selectedSeparator}
+            selectorClasses="w-[50%]"
+          />
+        </div>
+      </div>
       <Button
         text="Close modal"
         onPress={() => setIsOpen(false)}
--- src/features/type-select-modal/components/types-configurator/primitive-configurator/constants.ts
+++ src/features/type-select-modal/components/types-configurator/primitive-configurator/constants.ts
@@ -1,4 +1,4 @@
-export const uuidVersions = [
+const uuidVersions = [
   { id: 1, name: "UUIDV1" },
   { id: 2, name: "UUIDV3" },
   { id: 3, name: "UUIDV4" },
@@ -6,3 +6,18 @@ export const uuidVersions = [
   { id: 5, name: "UUIDV6" },
   { id: 6, name: "UUIDV7" },
 ];
+
+const dateFormat = [
+  { id: 1, name: "dd/mm/yy" },
+  { id: 2, name: "mm/dd/yy" },
+  { id: 3, name: "yy/dd/mm" },
+  { id: 4, name: "yy/mm/dd" },
+];
+
+const separators = [
+  { id: 1, name: "-" },
+  { id: 2, name: "." },
+  { id: 3, name: "/" },
+];
+
+export { uuidVersions, dateFormat, separators };
