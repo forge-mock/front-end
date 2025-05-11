b509ce834f9e4e4259b8aff744c43b9f749e7b22
Oleh Khavar <olehkhavar@gmail.com>
2025-05-11 23:38:09 +0300

    add title to account page

--- src/documents/account-page/AccountPage.tsx
+++ src/documents/account-page/AccountPage.tsx
@@ -3,7 +3,9 @@ import { Account } from "@widgets/account";
 
 function AccountPage(): React.JSX.Element {
   return (
-    <div className="flex">
+    <div className="flex flex-col items-center">
+      <h1 className="text-3xl font-bold my-5">User Profile</h1>
+
       <Account />
     </div>
   );
